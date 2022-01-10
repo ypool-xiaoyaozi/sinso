f63d64653f5b2a0ea4377e4fbfcc6349801abf65
xiaoyaozi <7920848+xiaoyaozi123@user.noreply.gitee.com>
2022-01-10 06:44:37 +0000

    update sin.sh.

--- sin.sh
+++ sin.sh
@@ -18,6 +18,6 @@ sed -i "/^debug-api-addr/{s/$/$number/}" /data/sinso$number/sinso$number.yaml
 sed -i "/^p2p-addr/{s/$/$number/}" /data/sinso$number/sinso$number.yaml
 sed -i "/^tracing-endpoint/{s/$/$number/}" /data/sinso$number/sinso$number.yaml
 sed -i "s/sinso/sinso$number/g" /data/sinso$number/sinso$number.yaml 
-cd $HOME/mine_publish-1.0.2 && sudo ./sinso start --config /data/sinso$number/sinso$number.yaml
-cd $HOME/mine_publish-1.0.2 && nohup sudo ./sinso start --config /data/sinso$number/sinso$number.yaml >/home/cat/mine_publish-1.0.2/sin$unmer.log 2>&1 &
+sinso start --config /data/sinso$number/sinso$number.yaml
+sinso start --config /data/sinso$number/sinso$number.yaml >/home/cat/sinso/sin$unmer.log 2>&1 &
 exit
