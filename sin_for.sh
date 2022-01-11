c61ab706065864c1903525edfc41d9dd228cf8e5
xiaoyaozi <7920848+xiaoyaozi123@user.noreply.gitee.com>
2022-01-11 07:23:42 +0000

    update sin_for.sh.

--- sin_for.sh
+++ sin_for.sh
@@ -2,5 +2,5 @@
 for i in {1..50}
 do
 	echo $i
-	sudo ./sinso start --config /data/sinso$i/sinso$i.yaml >sin$i.log 2>&1 &
+	sudo sinso start --config /data/sinso$i/sinso$i.yaml >sin$i.log 2>&1 &
 done
