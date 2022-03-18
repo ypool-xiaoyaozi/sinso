7a304c1bcffd7ebb1f5bf783f24deb1df288fdcf
xiaoyaozi <7920848+xiaoyaozi123@user.noreply.gitee.com>
2022-03-18 08:20:40 +0000

    update sin.sh. 修复脚本bug

--- sin.sh
+++ sin.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 read -p "enter a number: " number
 if [ -z $number ];then
-	echo "Error"
-	exit
+        echo "Error"
+        exit
 else
-	jieguo=`echo "$number*1" | bc `
-	if [ $jieguo -eq 0 ];then
-		echo "not a number"
-		exit
-	fi
+        jieguo=`echo "$number*1" | bc `
+        if [ $jieguo -eq 0 ];then
+                echo "not a number"
+                exit
+        fi
 fi
 [ -d "/data/sinso$number" ] && echo "目录存在" || mkdir /data/sinso$number
-if [ -n "`echo $number|sed 's/[0-9]//g'`" ]
+
+if [ -d "/data/sinso$number" ]
 then
-# mkdir /data/sinso$number 
-cp sinso.yml /data/sinso$number/sinso$number.yaml
-sed -i "/^api-addr/{s/$/$number/}" /data/sinso$number/sinso$number.yaml
-sed -i "/^debug-api-addr/{s/$/$number/}" /data/sinso$number/sinso$number.yaml
-sed -i "/^p2p-addr/{s/$/$number/}" /data/sinso$number/sinso$number.yaml
-sed -i "/^tracing-endpoint/{s/$/$number/}" /data/sinso$number/sinso$number.yaml
-sed -i "s/sinso/sinso$number/g" /data/sinso$number/sinso$number.yaml 
-sinso start --config /data/sinso$number/sinso$number.yaml
-else
-    "你输入的不是数字退出程序"
+        echo "前面输入的是数字，并且正常创建目录"
+        cp sinso.yml /data/sinso$number/sinso$number.yaml
+        sed -i "/^api-addr/{s/$/$number/}" /data/sinso$number/sinso$number.yaml
+        sed -i "/^debug-api-addr/{s/$/$number/}" /data/sinso$number/sinso$number.yaml
+        sed -i "/^p2p-addr/{s/$/$number/}" /data/sinso$number/sinso$number.yaml
+        sed -i "/^tracing-endpoint/{s/$/$number/}" /data/sinso$number/sinso$number.yaml
+        sed -i "s/sinso/sinso$number/g" /data/sinso$number/sinso$number.yaml
+        cd /root/mine_publish-1.1.0 && ./sinso start --config /data/sinso$number/sinso$number.yaml
 fi
-exit
